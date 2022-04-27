384a37c20ace7445a5643310e5f93d7f0773011e
Ritesh1511 <riteshpc12@gmail.com>
2022-04-27 23:20:24 +0530

    Initial commit

---
+++ components/ColorBox.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View,Text} from 'react-native';
+const ColorBox=()=>{
+    <View>
+        <Text>
+            I am ColorBox
+        </Text>
+    </View>
+};
+export default ColorBox;
